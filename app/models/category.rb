fc1a17bfb63b8ec4822ffcaa71a45c637baaabd4
ginohmk <michotall95@gmail.com>
2022-05-25 21:29:11 +0100

    Add More styling Fix production error Fix linters Customize email message

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ApplicationRecord
   has_many :categorygroups, dependent: :destroy
   has_many :groups, through: :categorygroups
 
-  validates :amount, presence: true
+  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :name, presence: true
 end
